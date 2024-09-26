7c2144490ddc3fd1dcf0bf82715ae68a6a4edc43
renatocrachmad <162373494+renatocrachmad@users.noreply.github.com>
2024-09-26 15:42:40 -0300

    cria o projeto JL

---
+++ JL/src/main/java/br/com/liberdade/JL/JlApplication.java
@@ -0,0 +1,13 @@
+package br.com.liberdade.JL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlApplication.class, args);
+	}
+
+}
